572421c42602177b864b85285eeac16c4424edab
ehrhorn <pqc735@alumni.ku.dk>
2020-03-12 18:06:38 +0000

    powershovel updates

--- powershovel/powershovel_plotting.py
+++ powershovel/powershovel_plotting.py
@@ -2,12 +2,25 @@ import plotly.graph_objects as go
 from plotly import tools
 import numpy as np
 
+
+def y_axis_print(metric):
+    if metric == 'energy':
+        y_axis_text = '(x<sub>reco</sub> - x<sub>true</sub>) / x<sub>true</sub>'
+    elif metric == 'azimuth' or metric == 'zenith':
+        y_axis_text = 'x<sub>reco</sub> - x<sub>true</sub> (rad)'
+    elif metric == 'time':
+        y_axis_text = 'x<sub>reco</sub> - x<sub>true</sub> (ns)'
+    elif metric == 'x' or metric == 'y' or metric == 'z':
+        y_axis_text = 'x<sub>reco</sub> - x<sub>true</sub> (m)'
+    return y_axis_text
+
+
 def plotly_2d_histograms(selected_run_data, selected_comparison_data, metric, bin_type, own_name, opponent_name, meta_data):
     x_axis_text = 'Event length' if bin_type == 'event_length' else 'log E (E / GeV)'
+    y_axis_text = y_axis_print(metric)
     H1 = selected_run_data[metric][bin_type]['2d_histogram']['hist']
     H2 = selected_comparison_data[metric][bin_type]['2d_histogram']['hist']
     y_edges = meta_data['bin_edges'][bin_type]['yedges']
-    print(selected_comparison_data[metric][bin_type]['percentiles']['high_percentile'])
     x_edges = meta_data['bin_edges'][bin_type][metric]['xedges']
     percentile_x_mids = meta_data[bin_type]['bin_mids']
     max_z_values = []
@@ -207,7 +220,9 @@ def plotly_2d_histograms(selected_run_data, selected_comparison_data, metric, bi
     fig['layout']['xaxis2'].update(title=x_axis_text)
     fig['layout']['yaxis1'].update(title='Error')
     fig['layout']['yaxis1'].update(range=[min(x_edges), max(x_edges)])
+    fig['layout']['yaxis1'].update(title=y_axis_text)
     fig['layout']['yaxis2'].update(title='Error')
+    fig['layout']['yaxis2'].update(title=y_axis_text)
     fig['layout']['yaxis2'].update(range=[min(x_edges), max(x_edges)])
     fig['layout']['coloraxis'].update(colorscale='Oranges')
 
@@ -287,6 +302,7 @@ def plotly_2d_histograms(selected_run_data, selected_comparison_data, metric, bi
 
 def plotly_error_comparison(selected_run_data, selected_comparison_data, metric, bin_type, own_name, opponent_name, meta_data, bin_no):
     x_axis_text = 'Event length' if bin_type == 'event_length' else 'log E (E / GeV)'
+    y_axis_text = y_axis_print(metric)
     bin_mids = meta_data[bin_type]['bin_mids']
     bin_widths = meta_data[bin_type]['bin_widths']
     bin_lengths = [ibin * 2 for ibin in bin_widths]
@@ -376,12 +392,12 @@ def plotly_error_comparison(selected_run_data, selected_comparison_data, metric,
             domain=[0, 0.6]
         ),
         xaxis2=dict(
-            title='Error',
+            title=y_axis_text,
             domain=[0.7, 1],
-            range=[
-                selected_run_data[metric][bin_type]['percentiles']['low_cut_percentile'][bin_no],
-                selected_run_data[metric][bin_type]['percentiles']['high_cut_percentile'][bin_no]
-            ]
+            # range=[
+            #     selected_run_data[metric][bin_type]['percentiles']['low_cut_percentile'][bin_no],
+            #     selected_run_data[metric][bin_type]['percentiles']['high_cut_percentile'][bin_no]
+            # ]
         ),
         yaxis=dict(
             title='(84th percentile - 16th percentile) / 2',
@@ -494,4 +510,158 @@ def plotly_error_comparison(selected_run_data, selected_comparison_data, metric,
                     dash='dot'
                 ),
             )
+    return fig
+
+
+def convert_to_cartesian(azimuth, zenith):
+    x = np.sin(zenith) * np.cos(azimuth)
+    y = np.sin(zenith) * np.sin(azimuth)
+    z = np.cos(zenith)
+    return np.array([[x, y, z]])
+
+
+def direction_vectors(direction, position, scale):
+    x = [position[0, 0] - direction[0, 0] * scale, direction[0, 0] * scale + position[0, 0]]
+    y = [position[0, 1] - direction[0, 1] * scale, direction[0, 1] * scale + position[0, 1]]
+    z = [position[0, 2] - direction[0, 2] * scale, direction[0, 2] * scale + position[0, 2]]
+    return np.array([x, y, z])
+
+
+def plotly_event(predictions, scalars, sequential, comparison, geom, selected_run, selected_comparison):
+    own_dirs = predictions[['predicted_primary_direction_x', 'predicted_primary_direction_y', 'predicted_primary_direction_z']].values
+    own_pos = predictions[['predicted_primary_position_x', 'predicted_primary_position_y', 'predicted_primary_position_z']].values
+
+    true_dirs = scalars[['true_primary_direction_x', 'true_primary_direction_y', 'true_primary_direction_z']].values
+    true_pos = scalars[['true_primary_position_x', 'true_primary_position_y', 'true_primary_position_z']].values
+
+    own_vector = direction_vectors(own_dirs, own_pos, 100000)
+    true_vector = direction_vectors(true_dirs, true_pos, 100000)
+
+    if selected_comparison == 'retro_crs_prefit':
+        opp_angles = scalars[['retro_crs_prefit_azimuth', 'retro_crs_prefit_zenith']].values
+        opp_dirs = convert_to_cartesian(opp_angles[0][0], opp_angles[0][1])
+        opp_pos = scalars[['retro_crs_prefit_x', 'retro_crs_prefit_y', 'retro_crs_prefit_z']].values
+    else:
+        opp_dirs = comparison[['predicted_primary_direction_x', 'predicted_primary_direction_y', 'predicted_primary_direction_z']].values
+        opp_pos = comparison[['predicted_primary_position_x', 'predicted_primary_position_y', 'predicted_primary_position_z']].values
+
+    opp_vector = direction_vectors(opp_dirs, opp_pos, 100000)
+
+
+    trace1 = go.Scatter3d(
+        x=sequential.dom_x.values,
+        y=sequential.dom_y.values,
+        z=sequential.dom_z.values,
+        mode='markers',
+        name='pulses',
+        marker=dict(
+            size=10,
+            color=sequential.dom_time.values,
+            colorscale='Viridis',
+            opacity=0.5
+        )
+    )
+
+    trace2 = go.Scatter3d(
+        x=geom.x.values,
+        y=geom.y.values,
+        z=geom.z.values,
+        mode='markers',
+        name='DOMs',
+        marker=dict(
+            size=1,
+            color='black',
+            opacity=0.5
+        )
+    )
+
+    trace3 = go.Scatter3d(
+        x=own_vector[0, :],
+        y=own_vector[1, :],
+        z=own_vector[2, :],
+        mode='lines',
+        name=selected_run,
+        marker=dict(
+            color='red',
+            size=0.1
+        )
+    )
+
+    trace4 = go.Scatter3d(
+        x=opp_vector[0, :],
+        y=opp_vector[1, :],
+        z=opp_vector[2, :],
+        mode='lines',
+        name=selected_comparison,
+        marker=dict(
+            color='green',
+            size=0.1
+        )
+    )
+
+    trace5 = go.Scatter3d(
+        x=true_vector[0, :],
+        y=true_vector[1, :],
+        z=true_vector[2, :],
+        mode='lines',
+        name='truth',
+        marker=dict(
+            color='blue',
+            size=0.1
+        )
+    )
+
+    trace6 = go.Scatter3d(
+        x=[own_pos[0, 0]],
+        y=[own_pos[0, 1]],
+        z=[own_pos[0, 2]],
+        mode='markers',
+        name=selected_run + ' vertex',
+        showlegend=False,
+        marker=dict(
+            color='red'
+        )
+    )
+
+    trace7 = go.Scatter3d(
+        x=[opp_pos[0, 0]],
+        y=[opp_pos[0, 1]],
+        z=[opp_pos[0, 2]],
+        mode='markers',
+        name=selected_comparison + ' vertex',
+        showlegend=False,
+        marker=dict(
+            color='green'
+        )
+    )
+
+    trace8 = go.Scatter3d(
+        x=[true_pos[0, 0]],
+        y=[true_pos[0, 1]],
+        z=[true_pos[0, 2]],
+        mode='markers',
+        name='true vertex',
+        showlegend=False,
+        marker=dict(
+            color='blue'
+        )
+    )
+
+    data = [trace1, trace2, trace3, trace4, trace5, trace6, trace7, trace8]
+    fig = go.Figure(data=data)
+    fig.update_layout(
+        scene = dict(
+            xaxis = dict(range=[-800, 800]),
+            yaxis = dict(range=[-800, 800]),
+            zaxis = dict(range=[-800, 800])
+        ),
+        height=800,
+        scene_aspectmode='cube',
+        legend=dict(
+            x=-.1,
+            y=1.2,
+            orientation='h'
+        )
+    )
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
     return fig
--- powershovel/powershovel_sql.py
+++ powershovel/powershovel_sql.py
@@ -9,7 +9,18 @@ class DbHelper:
         self.con = sqlite3.connect(sql_file)
         self.cursor = self.con.cursor()
 
-    def get_predictions(self, run_name, event_no):
+    def get_predictions(self, run_name, comparison_name, event_no):
         query = 'SELECT * FROM \'{}\' WHERE event = {}'.format(run_name, event_no)
         event_predictions = pd.read_sql_query(query, self.con)
-        return event_predictions
+        query = 'SELECT * FROM scalar WHERE event_no = {}'.format(event_no)
+        event_scalars = pd.read_sql_query(query, self.con)
+        query = 'SELECT * FROM sequential WHERE event_no = {}'.format(event_no)
+        event_sequential = pd.read_sql_query(query, self.con)
+        if comparison_name == 'retro_crs_prefit':
+            query = 'SELECT * FROM \'{}\' WHERE event = {}'.format(run_name, event_no)
+        else:
+            query = 'SELECT * FROM \'{}\' WHERE event = {}'.format(comparison_name, event_no)
+        event_comparison = pd.read_sql_query(query, self.con)
+        query = 'SELECT * FROM meta WHERE event_no = {}'.format(event_no)
+        event_meta = pd.read_sql_query(query, self.con)
+        return event_predictions, event_scalars, event_sequential, event_comparison, event_meta
