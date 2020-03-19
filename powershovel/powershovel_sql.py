699d4f9fc7f325dbd4996cace2fc9e98df31c608
ehrhorn <pqc735@alumni.ku.dk>
2020-03-19 08:16:48 +0000

    powershovel ammended for new metrics

--- powershovel/powershovel_sql.py
+++ powershovel/powershovel_sql.py
@@ -12,15 +12,15 @@ class DbHelper:
     def get_predictions(self, run_name, comparison_name, event_no):
         query = 'SELECT * FROM \'{}\' WHERE event = {}'.format(run_name, event_no)
         event_predictions = pd.read_sql_query(query, self.con)
-        query = 'SELECT * FROM scalar WHERE event_no = {}'.format(event_no)
+        query = 'SELECT * FROM scalar WHERE event = {}'.format(event_no)
         event_scalars = pd.read_sql_query(query, self.con)
-        query = 'SELECT * FROM sequential WHERE event_no = {}'.format(event_no)
+        query = 'SELECT * FROM sequential WHERE event = {}'.format(event_no)
         event_sequential = pd.read_sql_query(query, self.con)
         if comparison_name == 'retro_crs_prefit':
             query = 'SELECT * FROM \'{}\' WHERE event = {}'.format(run_name, event_no)
         else:
             query = 'SELECT * FROM \'{}\' WHERE event = {}'.format(comparison_name, event_no)
         event_comparison = pd.read_sql_query(query, self.con)
-        query = 'SELECT * FROM meta WHERE event_no = {}'.format(event_no)
+        query = 'SELECT * FROM meta WHERE event = {}'.format(event_no)
         event_meta = pd.read_sql_query(query, self.con)
         return event_predictions, event_scalars, event_sequential, event_comparison, event_meta
